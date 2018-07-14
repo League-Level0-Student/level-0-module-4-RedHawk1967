75dbf81c8daee3ef7be66dbc2d6602b5b7c7746c
Jack <league@WTS-IM-17.attlocal.net>
2018-07-13 17:55:16 -0700

    halloulahalloulahalloula

--- src/StringMethods1.java
+++ src/StringMethods1.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -7,17 +9,17 @@ public class StringMethods1 {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-
+String bob = "rxstdcgvynjiugvfjkhvhkgvkujhghtdxdfgszdsxcgfvhkjnhjdcfvkjhuyrdctytjghuvfhjhnigkklnjihbvl;kmlihgffcvhjgvhjnlvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvkmopl";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+		System.out.println(bob.charAt(3));
 		// 3. Print the length of your String to the console.
 		//    HINT: .length
-
+System.out.println(bob.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
-		
+for(int i=0; i<bob.length(); i++){ System.out.println(bob.charAt(i));  }
+for(int p=0; p<bob.length(); p++) {if(bob.charAt(p) == ';') {System.out.println( "; is at index"+ p );} 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
@@ -25,6 +27,6 @@ public class StringMethods1 {
 		//             for char 'b', then print "b is at index 1"
 		
 	}
-}
+} }
 
 
